a892256089320a0c14290d52c4bd9d772754b2e7
Jeremy DeLong <jeremydavid.delong@gmail.com>
2016-01-22 22:43:06 +0000

    that was weird

--- js/ma_gcs.js
+++ js/ma_gcs.js
@@ -1,200 +1,93 @@
-// Load the Visualization API and the chart package.
-google.load('visualization', '1', {
-	packages : ['corechart', 'table'],
-	callback : function() {
-		googleChartLoaded = true;
-	}
-});
-
 (function($) {
-
-	Drupal.behaviors.google_charts_shortcodes = {
-		attach : function(context, settings) {
-			//highchartsloaded = Drupal.settings.highchartsload.highchart;
-			//myColors = Drupal.settings.magcs.colors;
-			//console.log(myColors);
-			if ($('.shortcodes', context).length) {
-				$('.shortcodes').each(function(index) {
-
-					var mySpreadsheet = $(this).data('link');
-					var myChartType = $(this).data('type');
-					var myTitle = $(this).data('title');
-					var mySubTitle = $(this).data('subtitle');
-					var myLegal1 = $(this).data('legal1');
-					var myLegal2 = $(this).data('legal2');
-					var myStacked = $(this).data('isstacked');
-					ma_gcs.colors = Drupal.
-						console.log('my stacked = ' + myStacked);
-					if (mySpreadsheet.toLowerCase().indexOf('gid=') >= 0) {
-						myGid = mySpreadsheet.substr(mySpreadsheet.indexOf("gid=") + 4);
-						var myString = mySpreadsheet.substr(mySpreadsheet.indexOf("edit"));
-						//console.log(myGid);
-						//mySpreadsheet.replace(myString,'');
-						console.log(myString);
-						var myNewString = mySpreadsheet.replace(myString, "");
-						//console.log(myNewString);
-						mySpreadsheet = myNewString;
-						mySpreadsheet += 'gviz/tq';
-						if (myGid) {
-							mySpreadsheet += '?gid=' + myGid;
-						}
-
-					//console.log(mySpreadsheet);
-
-					}
-
-					var myurl = "https://" + mySpreadsheet;
-					var randomPie = $(this).data('id');
-					var colorvalue = [];
-					var thisColColor;
-					for (var i = 1; i < 9; i++) {
-						colorvalue.push($(this).data('col' + i));
-					//	console.log($(this).data('col' + i));
-					//	console.log(colorvalue);
-						//console.log(colorvalue[i]);
-					}
-					google.setOnLoadCallback(drawChart);
-					function drawChart() {
-						var opts = {
-							sendMethod : 'auto'
-						};
-						var query = new google.visualization.Query(myurl, opts);
-						query.send(handleQueryResponse);
-					}
-
-					function handleQueryResponse(response) {
-						if (response.isError()) {
-							alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-							return;
-						}
-						//var data = response.getDataTable();
-						var newData = response.getDataTable();
-
-						if(myChartType != 'Table') {
-
-							newData.addColumn({
-								type : 'string',
-								role : 'style'
-							});
-							var colCount = newData.getNumberOfColumns();
-							var rowCount = newData.getNumberOfRows();
-							var colorCount = colorvalue.length;
-							for (var i = 0; i < rowCount; i++) {
-								newData.setValue(i, colCount - 1, colorvalue[i % colorCount]);
-							}
-						}
-						var pieoptions = {
-							colors : colorvalue,
-              legend: { position: 'bottom' }
-							//  is3D: true
-						}
-
-						var baroptions = {
-							chartArea : {
-								width : '80%' //60%'
-							},
-              legend: { position: 'bottom' },
-              isStacked: myStacked
-							// hAxis : {
-								// title : 'Total Population',
-								// minValue : 0
-							// },
-							// vAxis : {
-								// title : 'City'
-							// }
-						};
-						var lineoptions = {
-							legend: { position: 'bottom'}
-						}
-						var chartoptions = {
-							chart: {
-								groupWidth : '80%' //70%'
-            	},
-							bar : {
-								groupWidth : '80%' //70%'
-							},
-							isStacked: myStacked,
-							legend: { position: 'bottom' }
-
-						}
-						var cssClassNames = {
-    				  'headerRow': 'headerRow',
-          		'tableRow': 'tableRow',
-    					'oddTableRow': 'oddTableRow',
-  						'selectedTableRow': 'selectedTableRow',
-					    'hoverTableRow': 'hoverTableRow',
-					    'headerCell': 'headerCell',
-					    'tableCell': 'tableCell',
-					    'rowNumberCell': 'rowNumberCell'
-			    	}
-
-  					var tableoptions = {
-  						width: '100%',
-  						height: '100%',
-              legend: { position: 'bottom' },
-  						'showRowNumber': false,
-  						'allowHtml': true,
-  						'cssClassNames': cssClassNames
-  					}
-
-						switch(myChartType) {
-						case 'PieChart':
-							var chart = new google.visualization.PieChart(context.getElementById(randomPie));
-							chart.draw(newData, pieoptions);
-//              $(window).on('resizeEnd', function(){
-//                chart.draw(newData, pieoptions);
-//              });
-							break;
-						case 'ColumnChart':
-							var chart = new google.visualization.ColumnChart(context.getElementById(randomPie));
-							chart.draw(newData, chartoptions);
-//              $(window).on('resizeEnd', function(){
-//                chart.draw(newData, chartoptions);
-//              });
-							break;
-						case 'BarChart':
-							var chart = new google.visualization.BarChart(context.getElementById(randomPie));
-							chart.draw(newData, baroptions);
-//              $(window).on('resizeEnd', function(){
-//                chart.draw(newData, baroptions);
-//              });
-							break;
-						case 'LineChart':
-							var chart = new google.visualization.LineChart(context.getElementById(randomPie));
-							chart.draw(newData, lineoptions);
-//              $(window).on('resizeEnd', function(){
-//                chart.draw(newData, lineoptions);
-//              });
-							break;
-						case 'Table':
-						  var table = new google.visualization.Table(context.getElementById(randomPie));
-  						  console.log(JSON.stringify(newData));
-  						  //console.log();
-  						  table.draw(newData, tableoptions);
-//  			    $(window).on('resizeEnd', function(){
-//                  table.draw(newData, tableoptions);
-//                });
-  						  //$(this).css( "height", "auto" );
-  						  thisis = '#' + randomPie;
-  						  //console.log(thisis);
-  						  $(thisis).css( "height", "auto" );
-  						  $('.google-visualization-table').css( "height", "auto" );
-  						  $( '<p class="rbfsubtext">' + myLegal2 + '</p>' ).insertAfter( $(context.getElementById(randomPie)) );
-						    $( '<p class="rbfsubtext">' + myLegal1 + '</p>' ).insertAfter( $(context.getElementById(randomPie)) );
-  						break;
-						}
-						$( '<h3 class="rbftitle">' + myTitle + '</h3>' ).insertBefore( $(context.getElementById(randomPie)) );
-						$( '<h4 class="rbfsubtitle">' + mySubTitle + '</h4>' ).insertBefore( $(context.getElementById(randomPie)) );
-						$('text').each(function() {
-								$(this).removeAttr( "font-family" );
-								$(this).removeAttr( "font-size" );
-								$(this).css("font-size","10");
-
-						});
-						//$( "text:contains('start')" ).css( "text-decoration", "underline" );
-					} //query response
-				}); //each function
-			} //if context.length
-		} //attach:function
-	} //Drupal.behavior
+  Drupal.behaviors.ma_gcs = {
+    attach : function(context, settings) {
+      if ($('.shortcodes', context).length) {
+        $('.shortcodes').each(function(index) {
+          var myChartType = $(this).data('type');
+          var myChartID = $(this).data('chartid');
+          var myChartGid = $(this).data('gid');
+          var myTitle = $(this).data('title');
+          var mySubTitle = $(this).data('subtitle');
+          var myLegal1 = $(this).data('legal1');
+          var myLegal2 = $(this).data('legal2');
+          var myStacked = $(this).data('isstacked');
+          var randomId = $(this).data('id');
+          var colorvalue = [];
+          var thisColColor;
+          for (var i = 1; i < 9; i++) {
+            colorvalue.push($(this).data('col' + i));
+          }
+          var myURL = "https://spreadsheets.google.com/feeds/list/" + myChartID + "/od6/public/values?gid=" + myChartGid + "&alt=json"; 
+          console.log(myURL);
+          $.getJSON(myURL, function (data) {
+            for (var i = 0; i < data.feed.entry.length; i++) {
+              for (var key in data.feed.entry[i]) {
+                if (data.feed.entry[i].hasOwnProperty(key) && key.substr(0,4) === 'gsx$') {
+                // copy the value in the key up a level and delete the original key
+                  data.feed.entry[i][key.substr(4)] = data.feed.entry[i][key].$t;
+                  delete data.feed.entry[i][key];
+                }
+                else
+                {
+                  delete data.feed.entry[i][key];
+                }
+              }
+              var datap = data.feed.entry;
+              switch (myChartType) {
+                case 'piechart' :
+                  var chart = c3.generate({
+                    bindto: document.getElementById(randomId),
+                    data: {
+                      json: datap,
+                      type: 'pie'
+                    },
+                    color: {
+                      pattern: colorvalue
+                    },
+                    legend: {
+                      position: 'bottom'
+                    }
+                  });
+                  break;
+                case 'columnchart' :
+                  var chart = c3.generate({
+                    bindto: document.getElementById(randomId),
+                    data: {
+                      json: datap,
+                      type: 'step'
+                    },
+                    color: {
+                      pattern: colorvalue
+                    },
+                    legend: {
+                      position: 'bottom'
+                    }
+                  });
+                  break;
+                case 'barchart' :
+                  var chart = c3.generate({
+                    bindto: document.getElementById(randomId),
+                    data: {
+                      json: datap,
+                      type: 'bar'
+                    },
+                    color: {
+                      pattern: colorvalue
+                    },
+                    legend: {
+                      position: 'bottom'
+                    }
+                  });
+                  break;
+                case 'linechart' :
+                  break;
+                case 'tablechart' :
+                  break;
+              }
+            }
+          });
+        }); // each function
+      } // if context.length
+    } // attach:function
+  } // Drupal.behavior
 })(jQuery);
--- test.php
+++ test.php
@@ -1,141 +1,57 @@
 <?php 
-
-function _ma_gcs_base_variables() {
-  return array(
-    'link' => '',
-    'title' => '',
-    'subtitle' => '',
-    'col1' => '',
-    'col2' => '',
-    'col3' => '',
-    'col4' => '',
-    'col5' => '',
-    'col6' => '',
-    'col7' => '',
-    'col8' => '',
-  );
-}    
-
-function ma_gcs_theme() {
-		
-  return array(
-    'shortcode_piechart' => array(
-      'variables' => _ma_gcs_base_variables(),   
-    ),
-  	'shortcode_barchart' => array(
-  		'variables' => _ma_gcs_base_variables(),
- # 			'isstacked' => '',
-  	),
-  		 
-  	'shortcode_columnchart' => array(
-  		'variables' => _ma_gcs_base_variables(),
- # 			'isstacked' => '',
-  	),
-  	'shortcode_tablechart' => array(
-  		'variables' => _ma_gcs_base_variables(),
-  #			'legal1' => '',
-  #			'legal2' => '',
-  	),	
-  );
+//$feed = docs.google.com/spreadsheet/ccc?key=<KEY>#gid=12345
+$feed = "https://spreadsheets.google.com/feeds/list/140f-u3YxCg3xlspx-xNWPbkXVPSHOOMopCWtsV-Wdao/od6/public/values?gid=308856877";
+header('Content-type: application/json');
+ 
+// Set your CSV feed
+//$feed = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0Akse3y5kCOR8dEh6cWRYWDVlWmN0TEdfRkZ3dkkzdGc&single=true&gid=0&output=csv';
+ 
+// Arrays we'll use later
+$keys = array();
+$newArray = array();
+ 
+// Function to convert CSV into associative array
+function csvToArray($file, $delimiter) { 
+  if (($handle = fopen($file, 'r')) !== FALSE) { 
+    $i = 0; 
+    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
+      for ($j = 0; $j < count($lineArray); $j++) { 
+        $arr[$i][$j] = $lineArray[$j]; 
+      } 
+      $i++; 
+    } 
+    fclose($handle); 
+  } 
+  return $arr; 
+} 
+ 
+// Do it
+$data = csvToArray($feed, ',');
+ 
+// Set number of elements (minus 1 because we shift off the first row)
+$count = count($data) - 1;
+ 
+//Use first row for names  
+$labels = array_shift($data);  
+ 
+foreach ($labels as $label) {
+  $keys[] = $label;
 }
-$array=ma_gcs_theme();
-print_r ($array);
-
-function _ma_gcs_chart_types() {
-	return array(
-			'Pie' => 'piechart',
-			'Bar' => 'barchart',
-			'Column' => 'columnchart',
-			'Table' => 'tablechart'
-	);
+ 
+// Add Ids, just in case we want them later
+$keys[] = 'id';
+ 
+for ($i = 0; $i < $count; $i++) {
+  $data[$i][] = $i;
 }
-function ma_gcs_shortcode_info() {
-	$chartarray=_ma_gcs_chart_types();
-	foreach ($chartarray as $key => $value) {
-		
-		$attcallback = 'ma_gcs_shortcode_' . $value . '_attributes';
-		$proccallback = 'ma_gcs_shortcode_' . $value . '';
-		$shortcodes[''. $value .''] = array(
-				'title' => ('Google ' . $key . ' Chart'),
-				'description' => ('Embed a Google ' . $key . ' Chart.'),
-				'attributes callback' => $attcallback,
-				'process callback' => $proccallback,
-				'tips callback' => 'ma_gcs_shortcode_tip'
-		);
-	}
-	return $shortcodes;
+ 
+// Bring it all together
+for ($j = 0; $j < $count; $j++) {
+  $d = array_combine($keys, $data[$j]);
+  $newArray[$j] = $d;
 }
-
-function ma_gcs_shortcode_barchart_attributes($form, &$form_state) {
-	$barform = _ma_gcs_formbuild('Bar');
-	$stacked = array(
-			'True' => t('True'),
-			'False' => t('False'),
-	);
-	$barform['barchart-isstacked']['#name'] = t('isstacked');
-	$barform['barchart-isstacked']['#title'] = t('Is this a Stacked Chart');
-	$barform['barchart-isstacked']['#type'] = 'select';
-	$barform['barchart-isstacked']['#description'] = t('this only applies to bar and column charts');
-	$barform['barchart-isstacked']['#options'] = $stacked;
-	$barform['barchart-isstacked']['#options']['#states'] = array(
-			'visible' => array(
-					':input[name="shortcode"]' => array('value' => 'barchart'),
-			),
-	);
-	return $barform;
-}
-
-print_r(ma_gcs_shortcode_info());
-$attrs = array (
-		
-);
-$ma_gcs_color1 = 'blue';
-$ma_gcs_color2 = 'green';
-$ma_gcs_color3 = 'red';
-$ma_gcs_color4 = 'orange';
-$ma_gcs_color5 = 'hazel';
-$ma_gcs_color6 = 'aqua';
-$ma_gcs_color7 = 'me';
-$ma_gcs_color8 = 'you';
-
-function _getcolors() {
-	$col = array();
-	for($x = 1; $x < 9; $x++) {
-		$col[$x] = "ma_gcs_color$x";
-	}
-	return $col;
-}
-function shortcode_attrs($pairs, $atts) {
-	$atts = (array)$atts;
-	$out = array();
-	foreach ($pairs as $name => $default) {
-		if ( array_key_exists($name, $atts) )
-			$out[$name] = $atts[$name];
-			else
-				$out[$name] = $default;
-	}
-	return $out;
-}
-function ma_gcs_shortcode_piechart() {
-	$attrs =  array (
-			'link' => '',
-			'title' => '',
-			'subtitle' => '',
-	);
-	for($x = 1; $x < 9; $x++) {
-		$attrs += array("col$x" => "ma_gcs_color$x");
-	}
-	
-	$themearray = array ();
-	foreach ($attrs as $key => $value) {
-		$themearray += array("$key" => "$value");
-	}
-	
-	//return theme ( 'shortcode_piechart', array ( $themearray ));
-	
-	return $themearray;
-}
-
-print_r (ma_gcs_shortcode_piechart());
-
-
+ 
+// Print it out as JSON
+echo json_encode($newArray);
+ 
+?>
