7e67d05e7ec5fb7d679f24b69300b14a5a36dd3a
Jeremy DeLong <jeremy@messageagency.com>
2016-05-02 12:52:53 -0400

    just a good place to stop

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,34 @@
+/**
+ * @file
+ */
+
 'use strict';
 
 var gulp = require('gulp');
 var phplint = require('phplint').lint;
 var phpcs = require('gulp-phpcs');
 var shell = require('gulp-shell'),
-pdependDocs = 'docs/pdepend';
+pdependDocs = './docs/pdepend';
+var phpcbf = require('gulp-phpcbf');
+var gutil = require('gutil');
+var phpmd = require('gulp-phpmd');
+
+
 var std = './vendor/drupal/coder/coder_sniffer/Drupal';
 
-
-
+//seems to work
 gulp.task('phplint', function (cb) {
     phplint(['./**/*.php', './**/*.module', './**/*.inc', '!node_modules/**/*', '!vendor/**/*'],  { limit: 10 }, function (err, stdout, stderr) {
-        if (err) {
-            cb(err);
-        } else {
-            cb();
-        }
+      if (err) {
+          cb(err);
+      }
+      else {
+          cb();
+      }
     });
 });
 
+//seems to work
 gulp.task('phpcs', function () {
     return gulp.src(['./**/*.php', './**/*.module', './**/*.inc', '!node_modules/', '!vendor/**/*'])
         .pipe(phpcs({
@@ -26,42 +36,55 @@ gulp.task('phpcs', function () {
             standard: std,
             warningSeverity: 0
         }))
-        .pipe(phpcs.reporter('log'));
+          .pipe(phpcs.reporter('log'));
 });
 
-gulp.task('phpcbf', 
-		shell.task(
-				['vendor/bin/phpcbf --standard='+std+' --ignore=vendor/,node_modules/ views/ templates/ ./'
-		]));
+//seems to work, but not a 100% sure
+gulp.task('phpcbf', function () {
+	return gulp.src(['./**/*.php', './**/*.module', './**/*.inc', '!node_modules/', '!vendor/**/*'])
+	    .pipe(phpcbf({
+	        bin: 'vendor/bin/phpcbf',
+	        standard: std,
+	        warningSeverity: 0
+	    }))
+	      .on('error', gutil.log)
+	      .pipe(gulp.dest('src'));
+});
 
-gulp.task('phpdoc', 
-		shell.task(
-				['vendor/bin/phpdoc -d . -t docs/phpdoc -i vendor/,node_modules/'
-		]));
+//Writing this without any special config because there is an xml config doc
+//seems to work
+gulp.task('phpdoc',
+        shell.task(['./vendor/bin/phpdoc']));
 
-gulp.task('pdepend', 
-		shell.task(
-				['mkdir -p ' + pdependDocs,
-				 'vendor/bin/pdepend --summary-xml=' + pdependDocs + '/summary.xml --jdepend-chart=' + pdependDocs + '/chart.svg --overview-pyramid=' + pdependDocs + '/pyramid.svg --ignore=vendor,node_modules --suffix=php --suffix=inc --suffix=modules.'
-		]));
+//nope
+gulp.task('pdepend',
+        shell.task(
+                ['mkdir -p ' + pdependDocs,
+                 './vendor/bin/pdepend --summary-xml=' + pdependDocs + '/summary.xml --jdepend-chart=' + pdependDocs + '/chart.svg --overview-pyramid=' + pdependDocs + '/pyramid.svg --ignore=vendor,node_modules --suffix=php,inc,modules .'
+        ]));
 
-gulp.task('phpmd', 
-		shell.task(
-				['vendor/bin/phpmd . codesize,unusedcode,naming,design,cleancode,controversial --reportfile docs/phpmd.html --exclude vendor/,node_modules/ --suffixes php modules inc'
-		]));
+//nope
+gulp.task('phpmd', function () {
+    return gulp.src(['./**/*.php', './**/*.module', './**/*.inc', '!node_modules/', '!vendor/**/*'])
+        // Validate code using PHP Mess Detector
+        .pipe(phpmd({
+            bin: 'vendor/bin/phpmd',
+            ruleset: 'cleancode,codesize,controversial,design,naming,unusedcode',
+            format: 'text',
+        }))
+        // Log all problems that was found
+        .on('error', gutil.log)
+        .pipe(phpmd.reporter('log'));
+});
 
 
 gulp.task('watch', function () {
     gulp.watch(['composer.json', './**/*.php', './**/*.modules', './**/*.inc', '!vendor/**/*', '!node_modules/**/*'], function (event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
-    //gulp.watch('composer.json', ['dump-autoload']);
+    // gulp.watch('composer.json', ['dump-autoload']);.
     gulp.watch(['./**/*.php', './**/*.modules', './**/*.inc', '!vendor/**/*', '!node_modules/**/*'], ['phplint', 'phpcs']);
 });
 
 
-gulp.task('default', ['phplint', 'phpcs', 'watch']);
-
-
-
-
+gulp.task('default', ['phplint', 'phpcs', 'phpcbf', 'phpdoc', 'watch']);
